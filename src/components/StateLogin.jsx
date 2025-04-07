1ab051127b566867971044e7a266dc05d87caf60
Dimitar <dimitar.mandarliev@gmail.com>
2025-04-07 10:06:53 +0300

    Implement email validation and error handling in Login component; add Signup component with form handling

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,10 +6,28 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
   function handleInputChange(identifier, value) {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -29,9 +47,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={(event) => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
